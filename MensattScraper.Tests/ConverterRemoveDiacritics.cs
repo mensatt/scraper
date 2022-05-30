7f28ed839a96e067872969139339e86b638ec5fe
stormofice <58337328+stormofice@users.noreply.github.com>
2022-05-30 23:50:42 +0200

    Introduce unit tests for diacritics removal

---
+++ MensattScraper.Tests/ConverterRemoveDiacritics.cs
@@ -0,0 +1,33 @@
+namespace MensattScraper.Tests;
+
+public class ConverterRemoveDiacritics
+{
+    [Theory]
+    [InlineData("ÄÖÜÊÉÈ", "AOUEEE")]
+    [InlineData("This is a tést", "This is a test")]
+    [InlineData("Pömmés Frîtès", "Pommes Frites")]
+    public void StringWithDiacritics(string input, string expected)
+    {
+        var result = input.RemoveDiacritics();
+
+        Assert.Equal(expected, result);
+    }
+
+    [Theory]
+    [InlineData("Please do not replace anything here!", "Please do not replace anything here!")]
+    [InlineData("°!§$%&/()=?`*',.-;:_*'«∑€∑®€†®Ω¨iøπ•æœ@∆ºª©ƒ∂", "°!§$%&/()=?`*',.-;:_*'«∑€∑®€†®Ω¨iøπ•æœ@∆ºª©ƒ∂")]
+    public void StringWithoutDiacritics(string input, string expected)
+    {
+        var result = input.RemoveDiacritics();
+
+        Assert.Equal(expected, result);
+    }
+
+    [Fact]
+    public void EmptyString()
+    {
+        var result = string.Empty.RemoveDiacritics();
+
+        Assert.Equal(string.Empty, result);
+    }
+}
