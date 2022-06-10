06b8cb1547056f6b6f0bedcc912086001fae347d
stormofice <58337328+stormofice@users.noreply.github.com>
2022-06-10 23:26:01 +0200

    Ensure fallback for DatabaseMappings
    This is mainly done to make CI unit testing possible, which currently
    fails due to the lack of an accompanying database.

--- MensattScraper/DatabaseSupport/DatabaseMapping.cs
+++ MensattScraper/DatabaseSupport/DatabaseMapping.cs
@@ -1,4 +1,5 @@
 ﻿using MensattScraper.DestinationCompat;
+using Npgsql;
 
 namespace MensattScraper.DatabaseSupport;
 
@@ -6,7 +7,7 @@ public static class DatabaseMapping
 {
     private static List<Location> _locations;
     private static List<Tag> _tags;
-    private static readonly IDatabaseWrapper DatabaseWrapper;
+    private static readonly IDatabaseWrapper? DatabaseWrapper;
 
     // I do not really like that we are using multiple potentially blocking calls in the static constructor at the moment
     // However making this a static class seems to be the best approach currently (to prevent passing an object through
@@ -15,10 +16,33 @@ public static class DatabaseMapping
     {
         _locations = new();
         _tags = new();
-        DatabaseWrapper =
-            new NpgsqlDatabaseWrapper("HOST=localhost;Port=8080;Username=mensatt;Password=mensatt;Database=mensatt");
-        DatabaseWrapper.ConnectAndPrepare();
-        RefreshDatabaseMappings();
+
+        try
+        {
+            DatabaseWrapper =
+                new NpgsqlDatabaseWrapper(
+                    "HOST=localhost;Port=8080;Username=mensatt;Password=mensatt;Database=mensatt");
+            DatabaseWrapper.ConnectAndPrepare();
+            RefreshDatabaseMappings();
+        }
+        catch (NpgsqlException)
+        {
+            // This *should* only happen in unit tests
+            DatabaseWrapper = null;
+            List<string> tagKeys = new()
+            {
+                "1",
+                "2", "4", "5", "7", "8", "9", "10", "11", "12", "13", "30", "S", "R", "G", "L", "W", "F", "V", "Veg",
+                "MSC",
+                "Gf", "CO2", "B", "MV", "Wz", "Ro", "Ge", "Hf", "Kr", "Ei", "Fi", "Er", "So", "Mi", "Man", "Hs", "Wa",
+                "Ka",
+                "Pe", "Pa", "Pi", "Mac", "Sel", "Sen", "Ses", "Su", "Lu", "We"
+            };
+            foreach (var tagKey in tagKeys)
+            {
+                _tags.Add(new(tagKey, string.Empty, string.Empty, null, Priority.UNSET, false));
+            }
+        }
     }
 
 
