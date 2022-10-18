0e6d4cb93f49866fe694d266c13fb728a4787cfe
stormofice <58337328+stormofice@users.noreply.github.com>
2022-10-18 23:14:35 +0200

    feat: Beautify webhook messages

--- MensattScraper/Telemetry/WebhookSender.cs
+++ MensattScraper/Telemetry/WebhookSender.cs
@@ -16,9 +16,18 @@ public class WebhookSender : ILogger
         Func<TState, Exception?, string> formatter)
         => _httpClient.PostAsync(WebhookUrl,
             new StringContent(
-                $"{{\"content\" : \"{DateTime.UtcNow.ToString("HH:mm:ss")} {logLevel}: {formatter.Invoke(state, exception)}\"}}",
+                $"{{\"content\" : \"{GenerateWrap(logLevel, $"{DateTime.UtcNow.ToString("HH:mm:ss")} {logLevel}: {formatter.Invoke(state, exception)}")}\"}}",
                 Encoding.UTF8,
                 "application/json"));
+
+    private static string GenerateWrap(LogLevel level, string x) => level switch
+    {
+        LogLevel.Trace or LogLevel.Debug or LogLevel.Information => $"```ini\\n[{x}]\\n```",
+        LogLevel.Warning => $"```http\\n{x}\\n```",
+        LogLevel.Error or LogLevel.Critical => $"```diff\\n-{x}\\n```",
+        LogLevel.None => x,
+        _ => throw new ArgumentOutOfRangeException(nameof(level), level, null)
+    };
 
     public bool IsEnabled(LogLevel logLevel) => throw new NotImplementedException();
     public IDisposable BeginScope<TState>(TState state) => throw new NotImplementedException();
