e3f1ba525dae903f4924c35cf1d6bceae0998af0
stormofice <58337328+stormofice@users.noreply.github.com>
2022-10-19 22:38:24 +0200

    fix: Squash concurrency problems

---
+++ MensattScraper/Telemetry/WebhookRelay.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections.Concurrent;
+
+namespace MensattScraper.Telemetry;
+
+public static class WebhookRelay
+{
+    public static readonly ConcurrentQueue<StringContent> Messages = new();
+    private static readonly HttpClient HttpClient = new();
+
+    static WebhookRelay()
+    {
+        new Thread(() =>
+        {
+            while (true)
+            {
+                if (Messages.IsEmpty)
+                    Thread.Sleep(5000);
+                else
+                {
+                    if (!Messages.TryDequeue(out var currentMessage)) break;
+                    HttpClient.PostAsync(WebhookUrl, currentMessage);
+                    Thread.Sleep(500);
+                }
+            }
+        }).Start();
+    }
+}
--- MensattScraper/Telemetry/WebhookSender.cs
+++ MensattScraper/Telemetry/WebhookSender.cs
@@ -6,46 +6,25 @@ namespace MensattScraper.Telemetry;
 
 public class WebhookSender : ILogger
 {
-    private readonly HttpClient _httpClient;
-    private readonly Queue<StringContent> _messages;
-    private bool _isTaskRunning;
-
     private readonly string _identifier;
 
     public WebhookSender(string identifier)
     {
-        _httpClient = new();
-        _messages = new();
         _identifier = identifier;
     }
 
     public void Log<TState>(LogLevel logLevel, EventId eventId, TState state, Exception? exception,
         Func<TState, Exception?, string> formatter)
     {
-        _messages.Enqueue(new(
+        WebhookRelay.Messages.Enqueue(new(
             $"{{\"content\" : \"{GenerateWrap(logLevel, (Debugger.IsAttached ? "[Debug] " : string.Empty) + $"{DateTime.UtcNow.ToString("HH:mm:ss")} {logLevel}: [{_identifier}]\\n{formatter.Invoke(state, exception)}")}\"}}",
             Encoding.UTF8,
             "application/json"));
-        if (!_isTaskRunning)
-        {
-            _isTaskRunning = true;
-            Task.Run(() =>
-            {
-                while (_messages.Count != 0)
-                {
-                    var currentMessage = _messages.Dequeue();
-                    _httpClient.PostAsync(WebhookUrl, currentMessage);
-                    Task.Delay(TimeSpan.FromMilliseconds(500)).Wait();
-                }
-
-                _isTaskRunning = false;
-            });
-        }
     }
 
     private static string GenerateWrap(LogLevel level, string x) => level switch
     {
-        LogLevel.Trace or LogLevel.Debug or LogLevel.Information => $"```ini\\n[{x}]\\n```",
+        LogLevel.Trace or LogLevel.Debug or LogLevel.Information => $"```ini\\n{x}\\n```",
         LogLevel.Warning => $"```fix\\n{x}\\n```",
         LogLevel.Error or LogLevel.Critical => $"```diff\\n-{x}\\n```",
         LogLevel.None => x,
