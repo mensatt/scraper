7e79510a800ecb5c2e27c53b7053934eb64eab53
Benedikt Nerb <nerb.benedikt@gmail.com>
2025-11-04 00:31:02 +0100

    Update table names to new DB tables

--- MensattScraper/DatabaseSupport/DatabaseConstants.cs
+++ MensattScraper/DatabaseSupport/DatabaseConstants.cs
@@ -2,33 +2,33 @@ namespace MensattScraper.DatabaseSupport;
 
 internal static class DatabaseConstants
 {
-    internal const string SelectIdByGermanNameSql = "SELECT id FROM dish WHERE name_de=@name_de";
+    internal const string SelectIdByGermanNameSql = "SELECT id FROM dishes WHERE name_de=@name_de";
 
     internal const string SelectDishIdByNormalizedAliasNameSql =
-        "SELECT dish FROM dish_alias WHERE normalized_alias_name=@normalized_alias_name";
+        "SELECT dish FROM dishes_aliases WHERE normalized_alias_name=@normalized_alias_name";
 
     internal const string SelectOccurrenceIdNameDateByLocationSql =
-        "SELECT id, dish, date, not_available_after FROM occurrence WHERE location=@location";
+        "SELECT id, dish, date, not_available_after FROM occurrences WHERE location=@location";
 
     internal const string SelectFullOccurrenceByLocationDateSql =
         "SELECT id, date, kj, kcal, fat, saturated_fat, carbohydrates," +
         " sugar, fiber, protein, salt, price_student, price_staff, price_guest," +
-        " dish, not_available_after FROM occurrence WHERE location=@location" +
+        " dish, not_available_after FROM occurrences WHERE location=@location" +
         " AND date>=@date";
 
     internal const string SelectOccurrenceTagsByIdSql =
-        "SELECT ot.tag FROM occurrence o JOIN occurrence_tags ot on o.id = ot.occurrence" +
+        "SELECT ot.tag FROM occurrence o JOIN occurrences_tags ot on o.id = ot.occurrence" +
         " WHERE o.id = @id";
 
-    internal const string SelectLocationIdNameLocationIdSql = "SELECT id, name, external_id FROM location";
+    internal const string SelectLocationIdNameLocationIdSql = "SELECT id, name, external_id FROM locations";
 
-    internal const string SelectTagAllSql = "SELECT key FROM tag";
+    internal const string SelectTagAllSql = "SELECT key FROM tags";
 
     internal const string InsertDishWithGermanNameSql =
-        "INSERT INTO dish (id, name_de, name_en) VALUES (@id, @name_de, @name_en) ON CONFLICT (name_de) DO NOTHING RETURNING id";
+        "INSERT INTO dishes (id, name_de, name_en) VALUES (@id, @name_de, @name_en) ON CONFLICT (name_de) DO NOTHING RETURNING id";
 
     internal const string InsertOccurrenceSql =
-        "INSERT INTO occurrence (id, location, dish, date, kj, kcal, fat, saturated_fat, " +
+        "INSERT INTO occurrences (id, location, dish, date, kj, kcal, fat, saturated_fat, " +
         "carbohydrates, sugar, fiber, protein, salt, price_student, " +
         "price_staff, price_guest) " +
         "VALUES (@id, @location, @dish, @date, @kj, @kcal, @fat, @saturated_fat, " +
@@ -36,22 +36,22 @@ internal static class DatabaseConstants
         "@price_staff, @price_guest) RETURNING id";
 
     internal const string InsertOccurrenceSideDishSql =
-        "INSERT INTO occurrence_side_dishes VALUES (@occurrence, @dish)";
+        "INSERT INTO occurrences_side_dishes VALUES (@occurrence, @dish)";
 
-    internal const string InsertOccurrenceTagSql = "INSERT INTO occurrence_tags VALUES (@occurrence, @tag)";
+    internal const string InsertOccurrenceTagSql = "INSERT INTO occurrences_tags VALUES (@occurrence, @tag)";
 
     internal const string InsertDishAliasSql =
-        "INSERT INTO dish_alias VALUES(@alias_name, @normalized_alias_name, @dish) RETURNING dish";
+        "INSERT INTO dishes_aliases VALUES(@alias_name, @normalized_alias_name, @dish) RETURNING dish";
 
     internal const string UpdateOccurrenceNotAvailableAfterByIdSql =
-        "UPDATE occurrence SET not_available_after=@not_available_after WHERE id=@id";
+        "UPDATE occurrences SET not_available_after=@not_available_after WHERE id=@id";
 
     internal const string UpdateOccurrenceContentsByIdSql =
-        "UPDATE occurrence SET kj=@kj, kcal=@kcal, fat=@fat, saturated_fat=@saturated_fat, " +
+        "UPDATE occurrences SET kj=@kj, kcal=@kcal, fat=@fat, saturated_fat=@saturated_fat, " +
         "carbohydrates=@carbohydrates, sugar=@sugar, fiber=@fiber, protein=@protein, salt=@salt, " +
         "price_student=@price_student, price_staff=@price_staff, price_guest=@price_guest " +
         "WHERE id=@id";
 
     internal const string DeleteOccurrenceTagByIdTagSql =
-        "DELETE FROM occurrence_tags WHERE occurrence=@id AND tag=@tag";
+        "DELETE FROM occurrences_tags WHERE occurrence=@id AND tag=@tag";
 }
